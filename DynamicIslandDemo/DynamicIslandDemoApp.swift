4f2bbb91152924939e03c8101a78449d134305d8
Seth Polyniak <emailspolyniak@gmail.com>
2022-12-05 09:04:00 -0500

    [main] - first commit

---
+++ DynamicIslandDemo/DynamicIslandDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicIslandDemoApp.swift
+//  DynamicIslandDemo
+//
+//  Created by Seth Polyniak on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct DynamicIslandDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
